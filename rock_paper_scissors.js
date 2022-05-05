17a756826907c4819303aae3000e9fbd928d998b
jbschklar <jbschklar@gmail.com>
2022-05-05 15:42:44 -0400

    refactored playerRound and game functions

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,30 +1,34 @@
 "use strict";
 
 const computerPlay = function () {
-	const choices = ["rock", "paper", "scissors"];
+	const choices = ["Rock", "Paper", "Scissors"];
 	const choice = choices[Math.floor(Math.random() * choices.length)];
 	return choice;
 };
 
 console.log(computerPlay());
 
-const playerRound = function (playerSelection, computerSelection) {
-	const a = playerSelection.toLowerCase();
-	const b = computerSelection.toLowerCase();
-
+const playerRound = function (a, b) {
+	const winA = `win! ${a} beats ${b}!`;
+	const winB = `lose! ${b} beats ${a}!`;
 	if (a === b) return "It's a draw!";
-	if (a === "rock" && b === "scissors") return "You win! Rock beats Scissors!";
-	if (a === "rock" && b === "paper") return "You lose! Paper beats Rock!";
-	if (a === "paper" && b === "rock") return "You win! Paper beats Rock!";
-	if (a === "paper" && b === "scissors")
-		return "You lose! Scissors beat Paper!";
-	if (a === "scissors" && b === "paper")
-		return "You win! Scissors beats Paper!";
-	if (a === "scissors" && b === "rock") return "You lose! Rock beats Scissors!";
+	if (a === "Rock") return `You ${b === "Scissors" ? winA : winB}`;
+	if (a === "Paper") return `You ${b === "Scissors" ? winB : winA}`;
+	if (a === "Scissors") return `You ${b === "Rock" ? winB : winA}`;
 };
 
 const game = function () {
-	let playerSelection = prompt("What's your choice? Rock, Paper, or Scissors?");
+	let x = prompt("What's your choice? Rock, Paper, or Scissors?");
+	let playerSelection = x[0].toUpperCase() + x.slice(1).toLowerCase();
+
+	if (
+		playerSelection !== "Rock" &&
+		playerSelection !== "Paper" &&
+		playerSelection !== "Scissors"
+	) {
+		alert("Invalid selection. Please choose rock, paper, or scissors :)");
+		location.reload();
+	}
 	let computerSelection = computerPlay();
 	console.log(playerSelection, computerSelection);
 	return playerRound(playerSelection, computerSelection);
