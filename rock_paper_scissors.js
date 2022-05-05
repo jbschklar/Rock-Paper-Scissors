1ad8e838eaef3f2977936b3c701afeea33acefc1
jbschklar <jbschklar@gmail.com>
2022-05-05 16:32:50 -0400

    added loop to game function and refactored

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -9,29 +9,46 @@ const computerPlay = function () {
 console.log(computerPlay());
 
 const playerRound = function (a, b) {
-	const winA = `win! ${a} beats ${b}!`;
-	const winB = `lose! ${b} beats ${a}!`;
+	const winPlayer = `win! ${a} beats ${b}!`;
+	const winComputer = `lose! ${b} beats ${a}!`;
 	if (a === b) return "It's a draw!";
-	if (a === "Rock") return `You ${b === "Scissors" ? winA : winB}`;
-	if (a === "Paper") return `You ${b === "Scissors" ? winB : winA}`;
-	if (a === "Scissors") return `You ${b === "Rock" ? winB : winA}`;
+	if (a === "Rock") return `You ${b === "Scissors" ? winPlayer : winComputer}`;
+	if (a === "Paper") return `You ${b === "Scissors" ? winComputer : winPlayer}`;
+	return `You ${b === "Rock" ? winComputer : winPlayer}`;
 };
 
 const game = function () {
-	let x = prompt("What's your choice? Rock, Paper, or Scissors?");
-	let playerSelection = x[0].toUpperCase() + x.slice(1).toLowerCase();
+	let compScore = 0;
+	let playerScore = 0;
 
-	if (
-		playerSelection !== "Rock" &&
-		playerSelection !== "Paper" &&
-		playerSelection !== "Scissors"
-	) {
-		alert("Invalid selection. Please choose rock, paper, or scissors :)");
-		location.reload();
+	for (let i = 0; i < 5; i++) {
+		let choice = prompt("What's your choice? Rock, Paper, or Scissors?");
+		let playerSelection =
+			choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+
+		if (
+			playerSelection !== "Rock" &&
+			playerSelection !== "Paper" &&
+			playerSelection !== "Scissors"
+		) {
+			alert("Invalid selection. Please choose rock, paper, or scissors :)");
+			// ++i;
+			continue;
+		}
+		let computerSelection = computerPlay();
+		console.log(playerSelection, computerSelection);
+
+		const x = playerRound(playerSelection, computerSelection);
+		console.log(x);
+		if (x.includes("win") && !x.includes("draw")) playerScore++;
+		if (x.includes("lose") && !x.includes("draw")) compScore++;
 	}
-	let computerSelection = computerPlay();
-	console.log(playerSelection, computerSelection);
-	return playerRound(playerSelection, computerSelection);
+	if (compScore !== playerScore)
+		return compScore > playerScore
+			? "You win the game!"
+			: "You lose! The computer beat you!";
+
+	return "It's a draw!";
 };
 
 console.log(game());
