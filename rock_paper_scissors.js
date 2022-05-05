bef7b3cd8413881759a60923143e47939f2b4295
jbschklar <jbschklar@gmail.com>
2022-05-05 13:03:24 -0400

    draft of playerRound and game functions

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -7,3 +7,27 @@ const computerPlay = function () {
 };
 
 console.log(computerPlay());
+
+const playerRound = function (playerSelection, computerSelection) {
+	const a = playerSelection.toLowerCase();
+	const b = computerSelection.toLowerCase();
+
+	if (a === b) return "It's a draw!";
+	if (a === "rock" && b === "scissors") return "You win! Rock beats Scissors!";
+	if (a === "rock" && b === "paper") return "You lose! Paper beats Rock!";
+	if (a === "paper" && b === "rock") return "You win! Paper beats Rock!";
+	if (a === "paper" && b === "scissors")
+		return "You lose! Scissors beat Paper!";
+	if (a === "scissors" && b === "paper")
+		return "You win! Scissors beats Paper!";
+	if (a === "scissors" && b === "rock") return "You lose! Rock beats Scissors!";
+};
+
+const game = function () {
+	let playerSelection = prompt("What's your choice? Rock, Paper, or Scissors?");
+	let computerSelection = computerPlay();
+	console.log(playerSelection, computerSelection);
+	return playerRound(playerSelection, computerSelection);
+};
+
+console.log(game());
