1d259b6e8b44cafdab0aec5bed2501ed5841b56e
jbschklar <jbschklar@gmail.com>
2022-05-13 15:58:41 -0400

    refactored original code to work with UI

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,14 +1,30 @@
 "use strict";
 
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
+const buttons = document.querySelectorAll(".btn");
+const results = document.querySelector(".results");
+const scores = document.querySelector(".scores");
+const round = document.querySelector(".current-round");
+const final = document.querySelector(".final");
+let playerScore = 0;
+let compScore = 0;
+
+const reset = function () {
+	playerScore = 0;
+	compScore = 0;
+};
+
 const computerPlay = function () {
 	const choices = ["Rock", "Paper", "Scissors"];
 	const choice = choices[Math.floor(Math.random() * choices.length)];
 	return choice;
 };
 
-console.log(computerPlay());
-
-const playerRound = function (a, b) {
+const playerRound = function (playerSelection) {
+	const a = playerSelection;
+	const b = computerPlay();
 	const winPlayer = `win! ${a} beats ${b}!`;
 	const winComputer = `lose! ${b} beats ${a}!`;
 	if (a === b) return "It's a draw!";
@@ -17,40 +33,31 @@ const playerRound = function (a, b) {
 	return `You ${b === "Rock" ? winComputer : winPlayer}`;
 };
 
-const game = function () {
-	let compScore = 0;
-	let playerScore = 0;
+buttons.forEach((button) => {
+	button.addEventListener("click", function (e) {
+		const playerSelection = e.target.textContent;
+		game(playerSelection);
+		final.textContent = finalResult(compScore, playerScore);
+	});
+});
 
-	for (let i = 0; i < 5; i++) {
-		let choice = prompt("What's your choice? Rock, Paper, or Scissors?");
-		let playerSelection =
-			choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+const finalResult = function (score1, score2) {
+	if (score1 === 5 || score2 === 5) {
+		reset();
+		if (score1 !== score2)
+			return score1 < score2
+				? "You win the game!"
+				: "You lose! The computer beat you!";
 
-		if (
-			playerSelection !== "Rock" &&
-			playerSelection !== "Paper" &&
-			playerSelection !== "Scissors"
-		) {
-			alert("Invalid selection. Please choose rock, paper, or scissors :)");
-			i--;
-			continue;
-		}
-		let computerSelection = computerPlay();
-		console.log(playerSelection, computerSelection);
-
-		const x = playerRound(playerSelection, computerSelection);
-		console.log(x);
-		if (x.includes("win") && !x.includes("draw")) playerScore++;
-		if (x.includes("lose") && !x.includes("draw")) compScore++;
-
-		console.log(compScore, playerScore);
+		return "It's a draw!";
 	}
-	if (compScore !== playerScore)
-		return compScore < playerScore
-			? "You win the game!"
-			: "You lose! The computer beat you!";
-
-	return "It's a draw!";
 };
 
-console.log(game());
+const game = function (playerSelection) {
+	const x = playerRound(playerSelection);
+	if (x.includes("win") && !x.includes("draw")) playerScore++;
+	if (x.includes("lose") && !x.includes("draw")) compScore++;
+
+	scores.textContent = `You: ${playerScore} Computer: ${compScore}`;
+	round.textContent = x;
+};
